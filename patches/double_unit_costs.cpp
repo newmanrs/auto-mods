7febb1e1baf9beefe552994bec3bd8d4a9214ddd
newmanrs <newmanrs@umich.edu>
2023-04-26 23:02:55 -0700

    change doubled to more generic scale cost functions

--- patches/double_unit_costs.cpp
+++ patches/double_unit_costs.cpp
@@ -12,28 +12,35 @@
 // migrate to a shared "costs.h"
 
 // typedef genie::ResourceUsage<int16_t, int16_t, int16_t> ResourceCost; // defined in random_costs.h
-ResourceCost toDoubledResourceCost(const ResourceCost &resourceCost) {
-    ResourceCost doubledCost;
-    doubledCost.Type = resourceCost.Type;
-    doubledCost.Amount = resourceCost.Amount * 2;
-    doubledCost.Flag = (bool)resourceCost.Flag;
-    return doubledCost;
+//ResourceCost toDoubledResourceCost(const ResourceCost &resourceCost) {
+ResourceCost scaledResourceCost(const ResourceCost &resourceCost, const float scale) {
+    ResourceCost scaledCost;
+    scaledCost.Type = resourceCost.Type;
+    scaledCost.Amount = resourceCost.Amount * scale;
+    scaledCost.Flag = (bool)resourceCost.Flag;
+    return scaledCost;
 }
 
-std::vector<ResourceCost> toDoubledResourceCosts(const std::vector<ResourceCost> &resourceCosts) {
-    std::vector<ResourceCost> doubledCosts;
-    doubledCosts.reserve(resourceCosts.size());
+std::vector<ResourceCost> scaledResourceCosts(const std::vector<ResourceCost> &resourceCosts, const float scale) {
+    std::vector<ResourceCost> scaledCosts;
+    scaledCosts.reserve(resourceCosts.size());
     for (const ResourceCost &resourceCost : resourceCosts) {
-        doubledCosts.push_back(toDoubledResourceCost(resourceCost));
+        scaledCosts.push_back(scaledResourceCost(resourceCost, scale));
     }
-    return doubledCosts;
+    return scaledCosts;
 }
 
+
+// TODO: Generic this to "scaleUnitCosts(genie::DatFile *df, scale), and then
+// implement "2" or whatever as doubleUnitCosts.  We should have separate building
+// and/or unit scaling.
 void doubleUnitCosts(genie::DatFile *df) {
     std::vector<int> unitIds;
 
     for (genie::Unit unit : df->Civs.at(0).Units) {
-        if (hasNaturalResourceCost(unit)) {
+        //if (hasNaturalResourceCost(unit) and unit.Type == genie::UnitType::UT_Building) { // works
+        if (hasNaturalResourceCost(unit) and unit.Type == genie::UnitType::UT_Creatable){  // Units
+            //std::cout << "Type" + unit.Type << std::endl;
             unitIds.push_back(unit.ID);
         }
     }
@@ -41,7 +48,7 @@ void doubleUnitCosts(genie::DatFile *df) {
     for (int unitId : unitIds) {
 
         std::vector<ResourceCost> res = df->Civs.at(0).Units.at(unitId).Creatable.ResourceCosts;
-        std::vector<ResourceCost> doubled = toDoubledResourceCosts(res);
+        std::vector<ResourceCost> doubled = scaledResourceCosts(res, 2.0); // Scale here
 
         std::cout << "Setting cost of unit with id " << unitId << " to " << costToString(doubled)
                   << std::endl;
